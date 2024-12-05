fd745bcaa1c8219c575c6711a8e08757d33bf191
musfikanoni <musfikanoni@gmail.com>
2024-12-05 22:50:32 +0600

    Delete coffee

--- src/CoffeeCard.jsx
+++ src/CoffeeCard.jsx
@@ -1,7 +1,40 @@
+import Swal from "sweetalert2";
 
 const CoffeeCard = ({ coffee }) => {
 
-    const {name, quantity, supplier,taste, category, details, photo} = coffee;
+    const {_id, name, quantity, supplier,taste, category, details, photo} = coffee;
+
+
+    const handleDelete = _id => {
+        console.log(_id);
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+
+            fetch(`http://localhost:5000/coffee/${_id}`, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.deletedCount > 0){
+                    Swal.fire({
+                        title: "Deleted!",
+                        text: "Your coffee has been deleted.",
+                        icon: "success"
+                      });
+                }
+            })
+            }
+          });
+    }
 
     return (
         <div className="card card-side bg-base-100 shadow-xl">
@@ -20,7 +53,8 @@ const CoffeeCard = ({ coffee }) => {
                     <div className="join join-vertical space-y-2">
                         <button className="btn join-item">View</button>
                         <button className="btn join-item">edit</button>
-                        <button className="btn join-item">x</button>
+                        <button onClick={() => handleDelete(_id)} 
+                         className="btn join-item">x</button>
                     </div>
                 </div>
             </div>
